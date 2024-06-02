50718a2800cc0023b62175ced72498d15d984175
David <davidezimafr@gmail.com>
2024-06-02 16:50:51 -0400

    Renamed main because of constructor issues

---
+++ sdfn.java
@@ -0,0 +1,14 @@
+
+public class sdfn{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+//
+// @kaitoti21
+// 2 years ago
+// Thank you, 
+// Maybe some suggestions, 
+//  - add the changes of github defaulting to "main" instead of master and the configs on it.
+//  - Setting up SSH keys(20:30) first must be earlier than the cloning through VS Code(14:30), so we can follow along
